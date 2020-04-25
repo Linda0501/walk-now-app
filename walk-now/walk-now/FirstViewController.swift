4a772ba100bd049fa152acd5dbeec7f3f7f9606f
Linda Deng <deng22@berkeley.edu>
2020-04-25 18:45:28 -0400

    new tabbed app

---
+++ walk-now/walk-now/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  walk-now
+//
+//  Created by Linda Deng on 2020-04-25.
+//  Copyright © 2020 Linda Deng. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
