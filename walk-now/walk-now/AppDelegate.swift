c2a984288f84d21ece1f59d35c2152a9b561e6c5
Linda Deng <deng22@berkeley.edu>
2020-04-25 23:02:35 -0400

    Sucessfully added dynamic street view of google map

--- walk-now/walk-now/AppDelegate.swift
+++ walk-now/walk-now/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyB5GVuibEtZKMvvKmWlA6EIRnglWFTzuFM")
+        //GMSPlacesClient.provideAPIKey("AIzaSyBTIm1akLunwWbfczd4-Da5i-XA4G1aiOk")
         return true
     }
 
--- walk-now/walk-now/SecondViewController.swift
+++ walk-now/walk-now/SecondViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import GoogleMaps
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, GMSMapViewDelegate {
 
+    @IBOutlet weak var panoramaView: GMSPanoramaView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        let panoView = GMSPanoramaView(frame: .zero)
+//        self.view = panoView
+//
+//        panoView.moveNearCoordinate(CLLocationCoordinate2D(latitude: -33.732, longitude: 150.312))
+//
+//
+        
+        panoramaView.moveNearCoordinate(CLLocationCoordinate2D(latitude: 37.3317134, longitude: -122.0307466))
+        
+
     }
 
 
