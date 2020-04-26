77420ffde1de08e9aa94d4657db395bb6dd70558
Linda Deng <deng22@berkeley.edu>
2020-04-25 20:24:21 -0400

    Accessed health data successfully

--- walk-now/walk-now/FirstViewController.swift
+++ walk-now/walk-now/FirstViewController.swift
@@ -10,36 +10,85 @@ import HealthKit
 
 class FirstViewController: UIViewController {
 
-    var available = 0 
-    var total = 0
+    var available = 0.0
+    var total = 0.0
     var lastDate = Date() 
 
     let healthKitStore: HKHealthStore = HKHealthStore()
 
-    func authorizeHealthKitinApp() {
-        let allTypes = Set([HKObjectType.quantityType(forIdentifier: .stepCount)!]) 
-
-        if !HKHealthStore.isHealthDataAvailable() {
-            print(“Error occurred”)
-            return
+    @IBAction func authoriseHealthKitAccess(_ sender: Any) {
+        let healthKitTypes: Set = [
+            // access step count
+            HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)!
+        ]
+        healthKitStore.requestAuthorization(toShare: healthKitTypes, read: healthKitTypes) { (_, _) in
+            print("authrised???")
         }
-
-        healthKitStore.requestAuthorization(toShare: allTypes, read: allTypes) { (success, error) in 
-            if !success {
-                print(“Error occured”)
+        healthKitStore.requestAuthorization(toShare: healthKitTypes, read: healthKitTypes) { (bool, error) in
+            if let e = error {
+                print("oops something went wrong during authorisation \(e.localizedDescription)")
+            } else {
+                print("User has completed the authorization flow")
             }
         }
-
     }
+//    func authorizeHealthKitinApp() {
+//        let allTypes = Set([HKObjectType.quantityType(forIdentifier: .stepCount)!])
+//
+//
+//        if !HKHealthStore.isHealthDataAvailable() {
+//            print("Error occured")
+//            return
+//        }
+//
+//        healthKitStore.requestAuthorization(toShare: allTypes, read: allTypes) { (success, error) in
+//            if !success {
+//                print("Error occured")
+//            }
+//        }
+//
+//    }
 
     // add IBOutlet for availableSteps and totalSteps UILabels 
 
-    func getSteps(completion: @escaping (Double) -> Void) {
+//    func getSteps(completion: @escaping (Double) -> Void) {
+//
+//        let stepsQuantityType = HKQuantityType.quantityType(forIdentifier: .stepCount)!
+//
+//        let now = Date()
+//        let predicate = HKQuery.predicateForSamples(withStart: lastDate, end: now, options: .strictStartDate)
+//
+//        let query = HKStatisticsQuery(quantityType: stepsQuantityType, quantitySamplePredicate: predicate, options: .cumulativeSum) { (_, result, error) in
+//            var resultCount = 0.0
+//            guard let result = result else {
+//                print("Failed to fetch steps rate")
+//                completion(resultCount)
+//                return
+//            }
+//            if let sum = result.sumQuantity() {
+//                resultCount = sum.doubleValue(for: HKUnit.count())
+//                self.available += resultCount
+//                self.total += resultCount
+//            }
+//
+//            //self.lastDate = now
+//            //self.totalSteps.text = total
+//            //self.availableSteps.text = available
+//
+//            DispatchQueue.main.async {
+//                completion(resultCount)
+//            }
+//        }
+//        self.healthKitStore.execute(query)
+//    }
+    
+    func getTodaysSteps(completion: @escaping (Double) -> Void) {
         
         let stepsQuantityType = HKQuantityType.quantityType(forIdentifier: .stepCount)!
         
         let now = Date()
-        let predicate = HKQuery.predicateForSamples(withStart: lastDate, end: now, options: .strictStartDate)
+        let startOfDay = Calendar.current.startOfDay(for: now)
+        let predicate = HKQuery.predicateForSamples(withStart: startOfDay, end: now, options: .strictStartDate)
         
         let query = HKStatisticsQuery(quantityType: stepsQuantityType, quantitySamplePredicate: predicate, options: .cumulativeSum) { (_, result, error) in
             var resultCount = 0.0
@@ -50,26 +99,35 @@ class FirstViewController: UIViewController {
             }
             if let sum = result.sumQuantity() {
                 resultCount = sum.doubleValue(for: HKUnit.count())
-			    available += resultCount
-                total += resultCount
             }
-		    
-            lastDate = now 
-            self.totalSteps.text = total
-            self.availableSteps.text = available
             
             DispatchQueue.main.async {
                 completion(resultCount)
             }
         }
-        healthStore.execute(query)
+        healthKitStore.execute(query)
     }
 
-      override func viewDidLoad() {
+    @IBOutlet weak var totalSteps: UILabel!
+    @IBAction func getTotalSteps(_ sender: Any) {
+        getTodaysSteps { (result) in
+            print("\(result)")
+            DispatchQueue.main.async {
+                self.totalSteps.text = "\(result)"
+            }
+        }
+    }
+    override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        authorizeHealthKitinApp()
-        getSteps()
+//        authorizeHealthKitinApp()
+//
+//    }
+//    getSteps { (result) in
+//        print("\(result)")
+//        DispatchQueue.main.async {
+//            self.totalSteps.text = "\(result)"
+//        }
     }
     
 }
